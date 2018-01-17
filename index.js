41aee9d01ab95bb8b30d44b27a79d709787dc5c7
Trevor Mullen <tbhackworth@gmail.com>
2018-01-17 21:12:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
 function theBeatlesPlay(musicians, instruments) {
-  var array = []
+  var arr = []
 
   for (var i = 0; i > musicians.length; i++) {
-    array.push(`${musicians[i]} plays ${instruments[i]}`)
+    arr.push(`${musicians[i]} plays ${instruments[i]}`)
   }
 
-  return array
+  return arr
+}
+
+function johnLennonFacts(facts) {
+  var arr = []
+  var i = 0
+
+  while (i < facts.length) {
+    arr.push(`${facts[i]}!!!`)
+    i++
+  }
+
+  return arr
 }
